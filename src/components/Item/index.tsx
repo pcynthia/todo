42ea10b2491fcad0aac68c3433b89bc007113d71
pcynthia <psoares@educacao.fortaleza.ce.gov.br>
2024-04-23 09:37:00 -0300

    feat: item todo

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -1,29 +1,47 @@
-import { Check, Pencil, Trash } from "@phosphor-icons/react"
-import styles from "./Item.module.css"
+import { Check, Pencil, Trash } from "@phosphor-icons/react";
+import { TaskProps } from "../../App";
+import styles from "./Item.module.css";
 
-export function Item() {
+interface ItemProps {
+  data: TaskProps;
+  onDeleteTask: (taskId: string) => void;
+  onToggleTaskStatus: ({ id, value }: { id: string; value: boolean }) => void;
+}
+
+export function Item({ data, onDeleteTask, onToggleTaskStatus }: ItemProps) {
+  const checkboxCheckedClassName = data.isChecked
+    ? styles["checkbox-checked"]
+    : styles["checkbox-unchecked"];
+
+  function handleTaskToggle() {
+    onToggleTaskStatus({ id: data.id, value: !data.isChecked });
+  }
+
+  function handleRemoveTask() {
+    onDeleteTask(data.id);
+  }
 
   return (
     <div className={styles.task}>
-     <button className={styles.checkContainer}>
-      <span className={styles.checkbox}>
-        <Check size={12} />
-      </span>
-     </button>
-     
-     <p className={styles.textChecked}>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-
-     <div className={styles.btnContainer}>
-      <button>
-        <Pencil className={styles.iconPencil} size={16} />
+      <button className={styles.checkContainer} onClick={handleTaskToggle}>
+        <span className={`${styles.checkbox} ${checkboxCheckedClassName}`}>
+          {data.isChecked && <Check size={12} />}
+        </span>
       </button>
 
-      <div className={styles.btn}>
+      <p className={data.isChecked ? styles.textChecked : ""}>{data.title}</p>
+
+      <div className={styles.btnContainer}>
         <button>
-          <Trash/>
+          <Pencil className={styles.iconPencil} size={16} />
         </button>
+
+        <div className={styles.btn}>
+          <button onClick={handleRemoveTask}>
+            <Trash size={16} />
+          </button>
+        </div>
       </div>
-     </div>
     </div>
-  )
+  );
 }
