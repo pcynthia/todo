52b840ccc48b8218cbd162ba9fe81ebf73cc151e
pcynthia <psoares@educacao.fortaleza.ce.gov.br>
2024-04-23 09:42:26 -0300

    cleanup: console

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,23 @@
-import { ChangeEvent, FormEvent, InvalidEvent, useState } from "react"
-import styles from "./Header.module.css"
-import { PlusCircle } from "@phosphor-icons/react"
+import { PlusCircle } from "@phosphor-icons/react";
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from "react";
+import styles from "./Header.module.css";
 
 interface HeaderProps {
   onAddTask: (taskTitle: string) => void;
 }
 
-export function Header({onAddTask}: HeaderProps) {
-  const [newTask, setNewTask] = useState("")
+export function Header({ onAddTask }: HeaderProps) {
+  const [newTask, setNewTask] = useState("");
 
   function handleCreateNewTask(event: FormEvent) {
     event.preventDefault();
-    onAddTask(newTask)
+    onAddTask(newTask);
     setNewTask("");
   }
 
   function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
     event.target.setCustomValidity("");
-    setNewTask(event.target.value)
-    console.log('newTask', newTask)
+    setNewTask(event.target.value);
   }
 
   function handleNewTaskInvalid(event: InvalidEvent<HTMLInputElement>) {
@@ -28,18 +27,18 @@ export function Header({onAddTask}: HeaderProps) {
   return (
     <header className={styles.header}>
       <form className={styles.newTaskForm} onSubmit={handleCreateNewTask}>
-        <input 
-          type="text" 
-          placeholder="Criar tarefa" 
+        <input
+          type="text"
+          placeholder="Criar tarefa"
           value={newTask}
           onChange={handleNewTaskChange}
           onInvalid={handleNewTaskInvalid}
           required
         />
         <button>
-          <PlusCircle size={20}/>
+          <PlusCircle size={20} />
         </button>
       </form>
     </header>
-  )
+  );
 }
