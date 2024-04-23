69ca260bfc98ac6a8a18f2ad684add1461ee84be
pcynthia <psoares@educacao.fortaleza.ce.gov.br>
2024-04-23 09:36:44 -0300

    feat: tasks

--- src/components/Tasks/index.tsx
+++ src/components/Tasks/index.tsx
@@ -1,36 +1,49 @@
-import { TaskProps } from '../../App';
-import { Item } from '../Item'
+import { TaskProps } from "../../App";
+import { Item } from "../Item";
+import { Empty } from "./Empty";
 
-import styles from './Tasks.module.css'
+import styles from "./Tasks.module.css";
 
 interface TasksProps {
   tasks: TaskProps[];
+  onDeleteTask: (taskId: string) => void;
+  onToggleTaskStatus: ({ id, value }: { id: string; value: boolean }) => void;
 }
 
-export function Tasks({tasks}: TasksProps) {
-  const tasksLength = tasks.length
+export function Tasks({ tasks, onDeleteTask, onToggleTaskStatus }: TasksProps) {
+  const tasksCounter = tasks.length;
+  const checkedTasksCounter = tasks.filter((task) => task.isChecked).length;
 
   return (
     <section className={styles.tasks}>
-      <header className={styles.header}>
-        <div>
-          <p>Tarefas adicionadas</p>
-          <span>{tasksLength}</span>
-        </div>
+      {tasksCounter > 0 && (
+        <header className={styles.header}>
+          <div>
+            <p>Tarefas adicionadas</p>
+            <span>{tasksCounter}</span>
+          </div>
 
-        <div>
-          <p className={styles.done}>Concluídas</p>
-          <span>3/4</span>
-        </div>
-      </header>
+          <div>
+            <p className={styles.done}>Concluídas</p>
+            <span>
+              {checkedTasksCounter} / {tasksCounter}
+            </span>
+          </div>
+        </header>
+      )}
 
-      {tasks.map(task => (
-        <p key={task.id}>{task.title}</p>
-      ))}
+      <div className={styles.list}>
+        {tasks.map((task) => (
+          <Item
+            key={task.id}
+            data={task}
+            onDeleteTask={onDeleteTask}
+            onToggleTaskStatus={onToggleTaskStatus}
+          />
+        ))}
 
-      <div>
-        <Item/>
+        {tasksCounter === 0 && <Empty />}
       </div>
     </section>
-  )
+  );
 }
